106da7f05bf6556c540aacf67d6941b61d541f56
Parley Martins <parleypachecomartins@gmail.com>
2017-11-21 18:40:19 -0200

    Improve UI

--- sales/resources/views/products/list.blade.php
+++ sales/resources/views/products/list.blade.php
@@ -2,15 +2,64 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <h1>TESTE TESTE</h1>
-                <ul>
-                @foreach ($products as $prod)
-                    <li><a href="{{ route('products.edit', $prod) }}">{{ $prod->name }}</a></li>
-                @endforeach
-            </ul>
+    <div class="panel panel-default">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-2">
+                <h1>Products</h1>
+            </div>
+            <div class="col-md-1">
+                <h1><a class="btn btn-primary" href="{{ route('products.create') }}">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                    New Product
+                </a></h1>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @if (count($products) === 0)
+                No products to show
+                @else
+                <table style="width:100%">
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    @foreach ($products as $prod)
+                    <tr>
+                        <td>
+                            <img src="{{ $prod->image }}" height="100" width="100" alt="{{ $prod->name }}-image">
+                        </td>
+                        <td>
+                            {{ $prod->name }}
+                        </td>
+                        <td>
+                            {{ $prod->price }}
+                        </td>
+                        <td>
+                            {{ $prod->category->name }}
+                        </td>
+                        <td>
+                            <a class="btn btn-default" href="{{ route('products.edit', $prod) }}">
+                                <i class="fa fa-pencil" aria-hidden="true"></i>
+                            </a>
+                        </td>
+                        <td>
+                            <form method="delete" action="{{ route('products.destroy', $prod) }}">
+
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                            </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+                @endif
             </div>
         </div>
     </div>
--- sales/resources/views/products/new.blade.php
+++ sales/resources/views/products/new.blade.php
@@ -2,45 +2,77 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+    <div class="panel panel-default">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
                 <h1>Product</h1>
-                    <form action="{{ $path_name }}" method="post" enctype="multipart/form-data">
-                        @if ($errors->any())
-                            <div class="alert alert-danger" role="alert">
-                                Please fix the following errors
-                                <ul>
-                                    @foreach($errors->all() as $error)
-                                        <li>{{$error}}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        {!! csrf_field() !!}
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{ old('name', $product->name) }}">
-
-                        <label for="image">Image</label>
-                        <input type="file" class="form-control" id="image" name="image" placeholder="Image" value="{{ old('image', $product->image) }}">
-
-                        <label for="price">Price</label>
-                        <input type="text" class="form-control" id="price" name="price" placeholder="Price" value="{{ old('price', $product->price) }}">
-
-                        <label for="category_id">Category_id</label>
-                        <select class="form-control" id="category_id" name="category_id" value="{{ old('category_id', $product->category_id) }}">
-                            @foreach($categories as $cat)
-                                <option value="{{$cat->id}}">{{$cat->name}}</option>
+                <form action="{{ $path_name }}" method="post" enctype="multipart/form-data">
+                    {{ $method }}
+                    @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        Please fix the following errors
+                        <ul>
+                            @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
                             @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                    {!! csrf_field() !!}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{ old('name', $product->name) }}">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="image">Image</label>
+                            <input type="file" class="form-control" id="image" name="image" placeholder="Image" value="{{ old('image', $product->image) }}">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="price">Price</label>
+                            <input type="text" class="form-control" id="price" name="price" placeholder="Price" value="{{ old('price', $product->price) }}">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="category_id">Category</label>
+                            <select class="form-control" id="category_id" name="category_id" value="{{ old('category_id', $product->category_id) }}">
+                                @foreach($categories as $cat)
+                                <option value="{{$cat->id}}">{{$cat->name}}</option>
+                                @endforeach
                             </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" id="description" name="description" placeholder="description">{{ old('description', $product->description) }}</textarea>
+                        </div>
+                    </div>
 
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-8">
 
-                        <label for="description">Description</label>
-                        <textarea class="form-control" id="description" name="description" placeholder="description">{{ old('description', $product->description) }}</textarea>
-
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
+                            <a href="{{ route('products.index') }}" class="btn btn-default">
+                                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                                Cancel
+                            </a>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-floppy-o" aria-hidden="true"></i>
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                @if($product->image)
+                Current Image
+                <img src="{{ $product->image }}" height="100" width="100" alt="{{ $product->name }}">
+                @endif
 
             </div>
         </div>
