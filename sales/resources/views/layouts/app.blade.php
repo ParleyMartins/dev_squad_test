71adad864c3873a5fcf743e1e4fd9928d1b12cbd
Parley Martins <parleypachecomartins@gmail.com>
2017-11-21 18:39:25 -0200

    Add fontawesome

--- sales/resources/views/layouts/app.blade.php
+++ sales/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://use.fontawesome.com/795776a6b2.js"></script>
 </head>
 <body>
     <div id="app">
